9bf4b451458dfd74f9d11d9abcf20ff367470d13
Raphael Marques <raphaeljcmarques@gmail.com>
2022-09-04 16:44:51 -0300

    adding the question, on task 01

--- Practice02/Ex01/Main.java
+++ Practice02/Ex01/Main.java
@@ -1,6 +1,19 @@
 package Practice02.Ex01;
 
-import Practice02.Ex01.ListaLigada;
+/*
+    Questão 01:
+    Uma agência bancária necessita organizar a fila para atendimento do
+    Auxílio Brasil. Cada usuário receberá uma senha numérica, serão
+    atendidas 200 pessoas por dia.
+    Percebeu-se que as pessoas que possuem senha impar até o número
+    100 são consideradas prioridade, por isso, devem ficar em outra fila
+    separada.
+
+    Fazer um programa usando lista que gere a entrada de dados de 200
+    pessoas, separe as pessoas com a senha impar até o número 100,
+    criando a lista de prioridades. Ao final deverão ser impressos a lista de
+    prioridade e a lista ordenada das pessoas que estão na fila comum.
+*/
 
 public class Main {
     public static void main(String[] args) {
